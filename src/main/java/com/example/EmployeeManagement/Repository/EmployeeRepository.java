ce2b529dd83929d6b9e490750873db14034f05e1
Aviwe <115289965+AC-Baleni@users.noreply.github.com>
2024-11-30 23:08:45 +0200

    Update EmployeeRepository.java

--- src/main/java/com/example/EmployeeManagement/Repository/EmployeeRepository.java
+++ src/main/java/com/example/EmployeeManagement/Repository/EmployeeRepository.java
@@ -3,7 +3,7 @@ package com.example.EmployeeManagement.Repository;
 import com.example.EmployeeManagement.Entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+// I'll be back
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
 }
